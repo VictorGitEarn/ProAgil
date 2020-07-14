01d0bec03f93081b8c510f664651742da5b1816b
Victor Alves <victor.earn@outlook.com>
2020-07-14 20:52:02 -0300

    Adicionado o ngx Bootstrap.

--- ProAgil-App/src/app/eventos/eventos.component.ts
+++ ProAgil-App/src/app/eventos/eventos.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { EventoService } from '../_services/evento.service';
+import { Evento } from '../_models/Evento';
 
 @Component({
   selector: 'app-eventos',
@@ -19,13 +20,13 @@ export class EventosComponent implements OnInit {
     this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this._filtroLista) : this.eventos;
   }
 
-  eventosFiltrados: any = [];
-  eventos: any = [];
+  eventosFiltrados: Evento[];
+  eventos: Evento[];
   imagemLergura = 50;
   imagemMargem = 2;
   mostrarImagem = false;
 
-  constructor(private http: HttpClient) { }
+  constructor(private eventoService: EventoService) { }
 
   // tslint:disable-next-line:typedef
   ngOnInit() {
@@ -37,7 +38,7 @@ export class EventosComponent implements OnInit {
     this.mostrarImagem = !this.mostrarImagem;
   }
 
-  filtrarEventos(filtrarPor: string): any{
+  filtrarEventos(filtrarPor: string): Evento[]{
     filtrarPor = filtrarPor.toLocaleLowerCase();
     return this.eventos.filter(
       evento => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1
@@ -46,8 +47,12 @@ export class EventosComponent implements OnInit {
 
   // tslint:disable-next-line:typedef
   getEventos() {
-    this.http.get('http://localhost:5000/Values').subscribe( response => {
-      this.eventos = response;
+    this.eventoService.getAllEvento().subscribe(
+      // tslint:disable-next-line:variable-name
+      (_eventos: Evento[]) => {
+      this.eventos = _eventos;
+      this.eventosFiltrados = this.eventos;
+      console.log(_eventos);
     }, error => {
       console.log(error);
     });
